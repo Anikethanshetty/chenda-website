4cb7dec3174ee9f04faecf24c3e9181df7a8b901
Anikethan D Shetty <163861494+Anikethanshetty@users.noreply.github.com>
2024-09-08 12:08:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,3 +34,17 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+const navTogglerBtn = document.querySelector(".nav-toggler")
+const aside = document.querySelector(".aside")
+
+navTogglerBtn.addEventListener("click",()=>{
+    asideSectionTogglerBtn()
+})
+
+function asideSectionTogglerBtn(){
+    aside.classList.toggle("open")
+    navTogglerBtn.classList.toggle("open")
+    for(let i=0;i<totalSection;i++){
+        allSection[i].toggle("open")
+    }
+}
